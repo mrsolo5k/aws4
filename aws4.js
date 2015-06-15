f83b414d71de048bedd2066ebbaaa9bd303b67bb
Piotr Lenarcik <piotr.lenarcik@gmail.com>
2015-06-15 21:26:00 +0200

    trying to add support for tts (ivona) service

--- aws4.js
+++ aws4.js
@@ -101,7 +101,7 @@ RequestSigner.prototype.sign = function() {
       if (this.credentials.sessionToken)
         headers['X-Amz-Security-Token'] = this.credentials.sessionToken
 
-      if (this.service === 's3')
+      if (this.service === 's3' || this.service === 'tts')
         headers['X-Amz-Content-Sha256'] = hash(this.request.body || '', 'hex')
 
       if (headers['X-Amz-Date'])
